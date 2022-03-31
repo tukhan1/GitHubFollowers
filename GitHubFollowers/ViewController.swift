3ffb69a5f6941bc2a3da5e0557b409b6a68c45d6
tukhan1 <tushev.ea@yandex.com>
2022-03-31 17:16:44 +0300

    Initial Commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Egor Tushev on 31.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
